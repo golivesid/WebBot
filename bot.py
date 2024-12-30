276d6ab40cdf25b2653717e75a871a01c5a17c93
Zinchenko vv <vlad.zinchenko.2012@mail.ru>
2024-12-30 15:16:02 +1000

    Finish

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import telebot
 from telebot import types
+import os
+from dotenv import load_dotenv
+import json
 
-bot = telebot.TeleBot('***')
+load_dotenv()
+bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 #dp = Dispatcher(bot)
 
 
@@ -10,12 +14,16 @@ def start(message):
     markup = types.ReplyKeyboardMarkup()
     webApp = types.WebAppInfo('https://tokotaty.github.io/WebBot/')
     markup.add(types.KeyboardButton('Открыть веб страницу', web_app=webApp))
-    bot.reply_to(message, 'Привет мой друг!', reply_markup = markup)
-
-#@bot.message_handler(conte=['start'])
+    bot.send_message(message.chat.id, 'Привет мой друг!', reply_markup = markup)
 
 
+@bot.message_handler(content_types=['web_app_data'])
+def web_app(message):
+    res = json.loads(message.web_app_data.data)
+    bot.send_message(message.chat.id, f'Name: {res["name"]}\nEmail: {res["email"]}\nPhone: {res["phone"]}')
 
 
-bot.infinity_polling(allowed_updates=['message', 'callback_query']) #для кнопок удаления и изменения "callback_messege"
-bot.polling(non_stop=True) #infiniti_polling непрерывная работа бота
+
+
+bot.infinity_polling(allowed_updates=['message', 'callback_query'])
+bot.polling(non_stop=True)
